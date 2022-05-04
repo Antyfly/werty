363c6a79e6288f9373b9743bd0648dcc5ff5479c
Юлия Бражникова <julia290102@mail.ru>
2022-05-04 14:37:04 +0300

    Добавьте файлы проекта.

---
+++ werty/Windows/MaterialList.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace werty.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MaterialList : Window
+    {
+        public MaterialList()
+        {
+            InitializeComponent();
+            MaterialListView.ItemsSource = new List<int> { 1, 1, 1, 1, 1, 1 };
+           
+        }
+    }
+}
